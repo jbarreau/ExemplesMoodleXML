183f6ef1bc29306c5adbe2fbb59a2ffd22599538
julien.barreau <julien.barreau@laposte.net>
2013-02-08 15:39:37 +0100

    main+listParam

--- Main.java
+++ Main.java
@@ -9,32 +9,22 @@ import tp1Project.Cyphezv2;
 
 
 public class Main {
-
+	private String threeWords = "one, two words...";
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//CypherPrinter.Component CP = CypherPrinter.newComponent(new CypherPrinterImpl());
-		CypherPrinter.Component CP = new CypherPrinter.Component(
-				new CypherPrinterImpl(),
-				new CypherPrinter.Bridge(){});
-		
-		CP.cypheAndPrint().send("azertypP");
-		
-		Cyphezv1.Component C1 = new Cyphezv1.Component(
-				new Cyphezv1Impl(),
-				new Cyphezv1.Bridge(){});
-		
-		C1.cyphez().send("cyphezc1" + "     " + "lqsdiugblqsdfbviqsdbql");
-		
-		Cyphezv2.Component C2 = new Cyphezv2.Component(
-				new Cyphezv2Impl(),
-				new Cyphezv2.Bridge(){});
-		
-		C2.cyphez().send("cyphezc2" + "     " + "lqsdiugblqsdfbviqsdbql");
-		
-		
-		
-		
+		julienListParam(args);
 	}
+
+
+	/**
+	* @author: julien
+	* to print on std out each param, separate by \n
+	*/
+	public static void julienListParam(String []args){
+		for (int i = 0 ; i < args.lenght() ; i++)
+			System.out.println(args[i]);
+	}
+	
 }
